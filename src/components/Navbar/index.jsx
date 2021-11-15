7043e2fc4ba24c5dd4e45e452c58022761332fcf
Rahul Parmar <rahulparmar.n@gmail.com>
2021-11-15 23:10:16 +0530

    added packages and navabar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,24 @@
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+const useStyles = makeStyles(() => ({
+    toolbar: {
+        margin: '0 auto',
+    },
+    h5: {
+        color: 'white',
+    },
+}));
+
+const Navbar = () => {
+    const classes = useStyles();
+    return (
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.h5}>
+                    Search Github Repos
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
+};
+
+export default Navbar;
